196862018a357bb9e767d92b9a5e21bdc767d3ea
ToRafin <66807542+ToRafin@users.noreply.github.com>
2020-07-18 04:54:37 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -11,12 +11,12 @@ canvas.addEventListener('submit',function createCanvas(event){
     const sizeHeight = document.getElementById('inputHeight').value;
     event.preventDefault();
     makeGrid(sizeHeight, sizeWidth);
-   
 }, false);
 
 
 function makeGrid(height, width) {
     let selectedColor = document.getElementById('colorPicker').value;
+    grid.innerHTML = null;
     for(let i = 0; i <height; i++){
         const squareRow = document.createElement('tr');
         grid.append(squareRow);
